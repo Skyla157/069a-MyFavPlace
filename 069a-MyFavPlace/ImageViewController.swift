ba0d46c9f58e4623445274e593f9ed8cd5d0fe80
Skyla157 <meagan157@gmail.com>
2016-04-24 22:42:42 -0700

    Changed photo page. Used my own code.

--- 069a-MyFavPlace/ImageViewController.swift
+++ 069a-MyFavPlace/ImageViewController.swift
@@ -1,98 +1,100 @@
 //
 //  ViewController.swift
-//  069-MyFavPlace
+//  069b-MyFavPlace
 //
-//  Created by Meagan McDonald on 4/20/16.
+//  Created by Meagan McDonald on 4/22/16.
 //  Copyright © 2016 Skyla157. All rights reserved.
 //
 
 import UIKit
 
-class ImageViewController: UIViewController {
-
+class ViewController: UIViewController {
     
-    @IBOutlet weak var pageControl: UIPageControl!
-    @IBOutlet weak var containerView: UIView!
-
+    @IBOutlet weak var photoTitleLbl: UILabel!
+    @IBOutlet weak var photoImg: UIImageView!
+    @IBOutlet weak var nextBtn: UIButton!
+    @IBOutlet weak var backBtn: UIButton!
+    @IBOutlet weak var numLbl: UILabel!
     
-    var imagePageViewController: ImagePageViewController? {
-        didSet {
-            imagePageViewController?.photoDelegate = self
-        }
-    }
+    var count: Int = 1
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        count = 1
         
-        pageControl.addTarget(self, action: "didChangePageControlValue", forControlEvents: .ValueChanged)
+        loadImageAndLabel(count)
     }
     
-    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        if let imagePageViewController = segue.destinationViewController as? ImagePageViewController {
-            self.imagePageViewController = imagePageViewController
+    @IBAction func onBackBtnPress(sender: AnyObject) {
+        decreaseInt(&count)
+        loadImageAndLabel(count)
+    }
+    
+    
+    @IBAction func onNextButtonPress(sender: AnyObject) {
+        increaseInt(&count)
+        loadImageAndLabel(count)
+    }
+    
+    func loadImageAndLabel(i: Int) {
+        switch i {
+        case 1:
+            photoImg.image = UIImage(named: "1StateStreet")
+            photoTitleLbl.text = "State Street"
+        case 2:
+            photoImg.image = UIImage(named: "2ButterflyBeach")
+            photoTitleLbl.text = "Butterfly Beach"
+        case 3:
+            photoImg.image = UIImage(named: "3SaddleRock")
+            photoTitleLbl.text = "Saddle Rock Loop"
+        case 4:
+            photoImg.image = UIImage(named: "4Zoo")
+            photoTitleLbl.text = "Santa Barbara Zoo"
+        case 5:
+            photoImg.image = UIImage(named: "5HendryBeach")
+            photoTitleLbl.text = "Hendry's Beach"
+        case 6:
+            photoImg.image = UIImage(named: "6SternsWarf")
+            photoTitleLbl.text = "Stearns Wharf"
+        case 7:
+            photoImg.image = UIImage(named: "7ChromaticGate")
+            photoTitleLbl.text = "The Chromatic Gate"
+        case 8:
+            photoImg.image = UIImage(named: "8ArroyoBurroBeach")
+            photoTitleLbl.text = "Arroyo Burro Beach"
+        case 9:
+            photoImg.image = UIImage(named: "9BotanicGarden")
+            photoTitleLbl.text = "Santa Barbara Botanic Garden"
+        case 10:
+            photoImg.image = UIImage(named: "10Wineries")
+            photoTitleLbl.text = "Wineries"
+        default:
+            print("Bad Number")
+        }
+        numLbl.text = "\(i) of 10"
+        
+    }
+    
+    func increaseInt(inout i: Int) {
+        if i == 10 {
+            i = 1
+        } else {
+            i = i + 1
         }
     }
     
-    //NEXT button
-    @IBAction func didTapNextButton(sender: UIButton) {
-        imagePageViewController?.scrollToNextViewController()
+    func decreaseInt(inout i: Int) {
+        if i == 1 {
+            i = 10
+        } else {
+            i = i - 1
+        }
     }
     
-    //BACK button
-    @IBAction func didTapBackButton(sender: UIButton) {
-        imagePageViewController?.scrollBackToLastController()
-    }
-    
-    /**
-     Fired when the user taps on the pageControl to change its current page.
-     */
-    func didChangePageControlValue() {
-        imagePageViewController?.scrollToViewController(index: pageControl.currentPage)
-    }
-}
-
-extension ImageViewController: ImagePageViewControllerDelegate {
-    
-    func imagePageViewController(imagePageViewController: ImagePageViewController,
-        didUpdatePageCount count: Int) {
-            pageControl.numberOfPages = count
-    }
-    
-    func imagePageViewController(imagePageViewController: ImagePageViewController,
-        didUpdatePageIndex index: Int) {
-            pageControl.currentPage = index
-    }
-    
-    @IBAction func tapBackToMain(sender: AnyObject) {
+    @IBAction func tapToGoBack(sender: AnyObject) {
         dismissViewControllerAnimated(true, completion: nil)
     }
+    
+    
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
